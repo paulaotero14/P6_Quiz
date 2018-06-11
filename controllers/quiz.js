dc6488cf6cebd494d301a7418ccac9191f2956b0
paulaotero14 <paula.otero.colmenar@alumnos.upm.es>
2018-06-11 20:34:55 +0200

    1

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -9,10 +9,7 @@ exports.load = (req, res, next, quizId) => {
 
     models.quiz.findById(quizId, {
         include: [
-            {
-                model: models.tip,
-                include: [ {model: models.user, as: 'author'} ]
-            },
+         
             {model: models.user, as: 'author'}    
         ]
     })
